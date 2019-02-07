640b1033f45f0ee6de12822d64440a45c1025130
Jeff Burke <potatopankakes@gmail.com>
2019-02-06 22:48:37 -0800

    skip logger creation if it is provided

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const gulp = require('gulp');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 require('godaddy-test-tools')(gulp, {
-  lint: { files: ['*.js', 'lib/**/*.js', 'test/*.js'] },
+  lint: { files: ['*.js', 'lib/**/*.js', 'test/**/*.js'] },
   istanbulReports: { dir: 'build' },
-  unitTestFiles: 'test/*.test.js'
+  unitTestFiles: 'test/**/*.test.js'
 });
